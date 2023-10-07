14af708e8f6cad3978ea092fe54297bdbdaff37f
Raoufbendii <mohamed.bendi@ensia.edu.dz>
2023-10-07 15:26:13 +0100

    stremlit

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from pydantic import BaseModel
+import joblib
+
+class PredictionInput(BaseModel):
+    PetalLengthCm: float
+    PetalWidthCm: float
+
+# Load your trained model
+model = joblib.load("randomForestIris.pkl")
+
+def predict(input_data):
+    prediction = model.predict([[input_data.PetalLengthCm, input_data.PetalWidthCm]])
+    return prediction[0]
+
+def main():
+    st.title("Position Lease Checker")
+    st.write("Enter Petal Length and Petal Width to predict.")
+
+    petal_length = st.text_input("Petal Length (cm):")
+    petal_width = st.text_input("Petal Width (cm):")
+
+    if st.button("Predict"):
+        input_data = PredictionInput(PetalLengthCm=float(petal_length), PetalWidthCm=float(petal_width))
+        prediction = predict(input_data)
+        st.write(f"Prediction: {prediction}")
+
+if __name__ == "__main__":
+    main()
