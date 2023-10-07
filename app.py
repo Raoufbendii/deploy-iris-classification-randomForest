ff779eb6697721db7bc4f52ed0d18c70ff42f2a2
Raoufbendii <mohamed.bendi@ensia.edu.dz>
2023-10-07 14:55:29 +0100

    v0.1

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template, jsonify
+from pydantic import BaseModel
+import joblib
+
+class PredictionInput(BaseModel):
+    PetalLengthCm: float
+    PetalWidthCm: float
+
+app = Flask(__name__)
+model = joblib.load("randomForestIris.pkl")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_data = request.get_json()
+    input_data = PredictionInput(**input_data)
+    prediction = model.predict([[input_data.PetalLengthCm, input_data.PetalWidthCm]])[0]
+    return jsonify({'prediction': prediction})
+
+if __name__ == "__main__":
+    app.run(port=8080)
